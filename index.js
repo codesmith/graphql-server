be1f3892160465b0e64f38cfa181b48f15340b7f
yonejima <catchandrelease9@gmail.com>
2021-10-24 20:56:16 +0900

    5章が終わるところまで実施済み。

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const Query = require('./resolver/Query');
 const Mutation = require('./resolver/Mutation')
 const Subscription = require('./resolver/Subscription');
 const typeDefs = require('./schema');
-
+// datamodel.prismaファイルから生成されたPrismaインスタンス  // ★追加
+const { prisma } = require('./prisma/generated/prisma-client')  // ★追加
 //PubSubのインスタンスを作成,サブスクリプションが利用可能に！ 
 const pubsub = new PubSub()
 
@@ -16,8 +17,9 @@ const server = new ApolloServer({
         Subscription,
     },
     context: {
-        db,
-        pubsub
+        prisma,  // ★追加
+        pubsub,
+        db
     }
 })
 
